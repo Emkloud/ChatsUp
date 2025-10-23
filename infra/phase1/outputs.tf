883a69e733211d97cd822f9a55d78f6ca02584e9
Emcloud <emma4real56@gmail.com>
2025-10-23 17:47:53 -0500

    correcting terraform error

--- infra/phase1/outputs.tf
+++ infra/phase1/outputs.tf
@@ -1,5 +1,5 @@
 output "api_public_ip" {
-  value = aws_instance.api.public_ip
+  value = length(aws_instance.api) > 0 ? aws_instance.api[0].public_ip : ""
 }
 
 output "turn_public_ip" {
@@ -7,7 +7,7 @@ output "turn_public_ip" {
 }
 
 output "api_ssh" {
-  value = "ssh ec2-user@${aws_instance.api.public_ip}"
+  value = length(aws_instance.api) > 0 ? "ssh ec2-user@${aws_instance.api[0].public_ip}" : ""
 }
 
 output "turn_ssh" {
